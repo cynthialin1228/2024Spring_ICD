51eccce8c184ff6f78b4e5783c85da32f5981dc2
cynthialin1228 <cynthialin1228@gmail.com>
2024-06-07 14:01:08 +0800

    final proj initialize

---
+++ ICD_FINAL/01_RTL/01_run.sh
@@ -0,0 +1,3 @@
+vcs -f rtl_01.f -R -full64 -debug_access+all +define+RTL+P1
+vcs -f rtl_01.f -R -full64 -debug_access+all +define+RTL+P2
+vcs -f rtl_01.f -R -full64 -debug_access+all +define+RTL+P3
---
+++ ICD_FINAL/01_RTL/interpolation.v
@@ -0,0 +1,17 @@
+module interpolation (
+    input           clk,
+    input           RST,
+    input           START,
+    input   [5:0]   H0,
+    input   [5:0]   V0,
+    input   [3:0]   SW,
+    input   [3:0]   SH,
+    output          REN,
+    input   [7:0]   R_DATA,
+    output  [11:0]  ADDR,
+    output  [7:0]   O_DATA,
+    output          O_VALID
+);
+
+
+endmodule
